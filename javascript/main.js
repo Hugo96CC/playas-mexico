f94dede69cbe180d0c1681ab7ef22c3404ba9e0f
Hugo96CC <108776151+Hugo96CC@users.noreply.github.com>
2022-11-29 16:46:59 -0600

    version

---
+++ javascript/main.js
@@ -0,0 +1,43 @@
+
+//DOM
+
+
+// Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+// Recorrelos
+
+links.forEach(function(links){
+
+    // Agregar un evento click a cada uno de ellos 
+
+    links.addEventListener("click",function(ev){
+        //Eliminamos el comportamiento por defecto
+        ev.preventDefault();
+
+        let content = document.querySelector('.content');
+
+        // Quitarle las clases de animacion que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        // Agregar clases para animar su salidad 
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        //setInterval
+
+        
+        setTimeout(function(){
+
+            location.href = "../index.html";
+
+        },600);// 600 milisegundos
+
+        
+
+        return false;
+
+    });
+});
